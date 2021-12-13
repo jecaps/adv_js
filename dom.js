4eabd121f10c60ad07915518897845257e26fa73
Jerika Kanz <jerikacapila@gmail.com>
2021-12-13 13:35:42 +0100

    Searching the DOM for Elements

--- dom.js
+++ dom.js
@@ -1,1 +1,4 @@
-document.body.children[1].children[0].href = "https:/google.com"; // the href attribute of a (anchor element) child of p (paragraph element) which is a child of the body element
+// document.body.children[1].children[0].href = "https:/google.com"; // the href attribute of a (anchor element) child of p (paragraph element) which is a child of the body element
+
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://google.com";
