dabe5483bd4d545a544e73582ed694326428f61d
Jerika Kanz <jerikacapila@gmail.com>
2021-12-13 15:31:55 +0100

    Inserting New HTML Elements via Javascript

--- dom.js
+++ dom.js
@@ -3,5 +3,20 @@
 let anchorElement = document.getElementById("external-link");
 anchorElement.href = "https://google.com";
 
-anchorElement = document.querySelector("#external-id"); // can select elements in different ways (element tag, classes, or id)
+anchorElement = document.querySelector("#external-link"); // can select elements in different ways (element tag, classes, or id)
 anchorElement.href = "https://academind.com";
+
+// Add an element
+// 1. Create the new element
+
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://google.com";
+newAnchorElement.textContent = "This leads to Google!";
+
+// 2. Get access to the parent element that should hold the new element
+
+let firstParagraph = document.querySelector("p");
+
+// 3. Insert the new element into the parent element content
+
+firstParagraph.append(newAnchorElement);
