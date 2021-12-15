c1ce10a5e6545bda5aec9bb6bcf0f3ae9819b3cb
Jerika Kanz <jerikacapila@gmail.com>
2021-12-15 19:52:24 +0100

    The "event" Object

--- event-listener.js
+++ event-listener.js
@@ -1,17 +1,21 @@
 let paragraphElement = document.querySelector("p");
 
-function changeParagraphText() {
+function changeParagraphText(event) {
   paragraphElement.textContent = "Clicked!";
   console.log("Paragraph clicked!");
+  console.log(event);
 }
 
 paragraphElement.addEventListener("click", changeParagraphText);
 
 let inputElement = document.querySelector("input");
 
-function retrieveUserInput() {
-  let enteredText = inputElement.value;
+function retrieveUserInput(event) {
+  // let enteredText = inputElement.value;
+  let enteredText = event.target.value; // shows every charachter added
+  // let enteredText = event.data; // shows only the character added => This is different from the two methods above
   console.log(enteredText);
+  console.log(event);
 }
 
 inputElement.addEventListener("input", retrieveUserInput);
