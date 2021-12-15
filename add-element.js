48ba30a43a54a82334da8227f57e1bdf816cc7ec
Jerika Kanz <jerikacapila@gmail.com>
2021-12-15 16:05:55 +0100

    Moving notes from to another file

---
+++ add-element.js
@@ -0,0 +1,14 @@
+// Add an element
+// 1. Create the new element
+
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://google.com";
+newAnchorElement.textContent = "This leads to Google!";
+
+// 2. Get access to the parent element that should hold the new element
+
+let firstParagraph = document.querySelector("p");
+
+// 3. Insert the new element into the parent element content
+
+firstParagraph.append(newAnchorElement);
