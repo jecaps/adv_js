992517fa6f3ed2f3a141cb6f56dd979eb1a09f68
Jerika Kanz <jerikacapila@gmail.com>
2021-12-15 16:25:02 +0100

    Deleting DOM Elements

---
+++ remove-elements.js
@@ -0,0 +1,9 @@
+// REMOVE ELEMENTS
+// 1. Select the elements that should be removed
+
+let firsH1Element = document.querySelector("h1");
+
+// 2. Remove it!
+
+// firsH1Element.parentElement.removeChild(firsH1Element); // this method of removing elements works on older browsers
+firsH1Element.remove(); // for modern browsers
