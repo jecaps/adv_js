caa7b02435c98fd2cd9aee5dac4389edad4be196
Jerika Kanz <jerikacapila@gmail.com>
2021-12-16 16:22:01 +0100

    Changing Element Styles With JavaScript

---
+++ elem-styles.js
@@ -0,0 +1,4 @@
+const spanElement = document.getElementById("remaining-chars");
+spanElement.textContent = "59";
+
+spanElement.style.color = "rgba(0, 120, 50)"; // note: use camelCase when styling using JavaScript. example : backgroundColor, textAlign etc.
