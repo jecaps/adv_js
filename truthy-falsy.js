3cab580706ae234f6eecd57d492663d27142e749
Jerika Kanz <jerikacapila@gmail.com>
2021-12-17 16:18:25 +0100

    "Truthy" & "Falsy" Values

---
+++ truthy-falsy.js
@@ -0,0 +1,10 @@
+const enteredUserName = "Jerika";
+
+// if (enteredName.length > 0) {
+if (enteredName) {
+  // this code and the above code both yields to true
+  console.log("Input is valid!");
+}
+
+// empty strings (""), 0, false, null, undefined are considered falsy values
+// values that are not of those above (like true) are truthy values
