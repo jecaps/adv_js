5cb1a81940fb2bc3927373d43d805055d3218ffe
Jerika Kanz <jerikacapila@gmail.com>
2021-12-20 14:32:20 +0100

    Practice Time: The "for...of loop"

--- exercise-3/loops.js
+++ exercise-3/loops.js
@@ -19,3 +19,19 @@ function calculateSum() {
 }
 
 calculateSumButtonElement.addEventListener("click", calculateSum);
+
+// Second Exampls: Highlight Links
+
+const highlightLinksButtonElement = document.querySelector(
+  "#highlight-links button"
+);
+
+function highlightLinks() {
+  const anchorElements = document.querySelectorAll("#highlight-links a");
+
+  for (const anchorElement of anchorElements) {
+    anchorElement.classList.add("highlight");
+  }
+}
+
+highlightLinksButtonElement.addEventListener("click", highlightLinks);
